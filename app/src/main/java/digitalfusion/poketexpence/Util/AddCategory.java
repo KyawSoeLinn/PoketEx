eddbe0c030ca1b7392ec9c9a91cf4f0fd5d3af47
KyawSoeLinn <nakashi94@gmail.com>
2018-05-11 10:08:09 +0630

    add 3
    add 3

--- app/src/main/java/digitalfusion/poketexpence/Util/AddCategory.java
+++ app/src/main/java/digitalfusion/poketexpence/Util/AddCategory.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +16,7 @@ import digitalfusion.poketexpence.Activity.Category;
 import digitalfusion.poketexpence.Data.DataBaseHelper;
 import digitalfusion.poketexpence.R;
 
-public class AddCategory extends Activity {
+public class AddCategory extends AppCompatActivity {
 
     EditText CatEdittxt;
     Button btncatAdd, btncatCancel;
@@ -71,5 +74,33 @@ public class AddCategory extends Activity {
         });
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
